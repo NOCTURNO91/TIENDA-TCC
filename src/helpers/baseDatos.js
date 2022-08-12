e2d58a2487d4b6b79c32345f9c1c4498c8727e63
unknown <NOCTURNO91>
2022-08-11 23:37:37 -0500

    se trabaja en la pagina

--- src/helpers/baseDatos.js
+++ src/helpers/baseDatos.js
@@ -16,7 +16,7 @@ export let productosBD=[
         precio:5000000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/apache1.jpg?alt=media&token=126728fe-bef0-48cc-9764-0ae5f25b9e7a','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/apache2.jpg?alt=media&token=bd1881c6-9569-4146-81d1-68006ad9a342'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'La Tuwan es una divertida bicicleta híbrida hecha para aventuras en bicicleta durante todo el día que te permitirá ampliar tus horizontes.',
         popularidad:3
     },
     {
@@ -24,7 +24,7 @@ export let productosBD=[
         precio:6000000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/pinarelo1.jpg?alt=media&token=3536dc73-c1d4-4a96-a09e-456ede8b4e93','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/pinarelo2.jpg?alt=media&token=280139dd-8359-4e2f-8693-efd9263a0957'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'El cuadro es menos extremo que el de la DOGMA F8 pero tiene las mismas características que lo hacen único: asimetría del cuadro, aunque menos extremista que F8, y un nuevo tipo de carbono de alta resistencia, que es ligeramente menos rígido.',
         popularidad:4
     },
     {
@@ -32,7 +32,7 @@ export let productosBD=[
         precio:4500000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/giant1.jpg?alt=media&token=50c32812-0198-49f2-bac2-21941c257e9e','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/giant2.jpg?alt=media&token=94e6f3fd-22ee-4b9b-b7fc-e286df876619'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'El cuadro Reign Advanced Pro 29 utiliza las tecnologías Advanced y Advanced Forged Composite desarrolladas por Giant. ',
         popularidad:5
     },
     {
@@ -40,7 +40,7 @@ export let productosBD=[
         precio:28000000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/marin1.jpg?alt=media&token=7b074d25-f2cc-4460-b1d6-4aad02aae625','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/marin2.jpg?alt=media&token=1e4cd487-50ac-4ba2-8af0-6297e1f4abac'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'Serie 4 Aluminio 6061, ruedas de 29 «, marco de aluminio superligero, freno trasero de montaje plano, enrutamiento interno de la palanca de cambios y la tija telescópica, eje pasante Boost de 148×12 mm',
         popularidad:4
     },
     {
@@ -48,7 +48,7 @@ export let productosBD=[
         precio:11055000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/mondraker1.jpg?alt=media&token=b1e0ef71-0184-4b27-9573-c9653f855b71','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/mondraker2.jpg?alt=media&token=f88750fd-225d-4f20-aefd-f3b0073e29b2'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'Tenemos bicicletas eléctricas de montaña de alta gama de la marca Mondraker, con motor de última generación Bosch y una autonomía de 70 km en modo e-MTB, 100 km en modo tour y 120 km en modo eco.',
         popularidad:5
     },
     {
@@ -56,7 +56,7 @@ export let productosBD=[
         precio:5123445000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/santacruz1.jpg?alt=media&token=54f51452-8ab4-4272-8095-b27fa190f8de','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/santacruz2.jpg?alt=media&token=9b48e77c-eb0d-4635-8b86-57422bdfa523'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'La Megatower es la fusión de ruedas grandes y el sistema de suspensión más impactante. Es un luchador moderno, tan adecuado para los corredores acérrimos como para los ciclistas que desean conquistar los senderos de su ciudad natal',
         popularidad:5
     },
     {
@@ -64,7 +64,7 @@ export let productosBD=[
         precio:50005000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/scott1.jpg?alt=media&token=cedaeea9-e0d7-4fbb-9016-36aab77cd3b0','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/scott2.jpg?alt=media&token=607837d3-9500-42e2-9a57-87a42343c36e'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'Bicicletas Scott fábrica múltiples modelos para diversas disciplinas dentro del ciclismo, utilizando principalmente aluminio y la fibra de carbono para la manufactura de todos sus cuadros, en tanto que para bicicletas Scott de ruta en sus distintas modalidades',
         popularidad:5
     },
     {
@@ -72,7 +72,7 @@ export let productosBD=[
         precio:500115000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/specialized1.jpg?alt=media&token=a7cd7598-4862-41d1-8c1c-d62e17aa35d5','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/specialized2.jpg?alt=media&token=f84a01e6-4312-4760-94ae-0cc51afdd913'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'Bicicleta Specialized Pitch rin 27.5, Talla M (17), con grupo Shimano Altus-Acera triplato de 9vel, suspensión Sr. Suntour XCM con bloqueo al hombro',
         popularidad:5
     },
     {
@@ -80,7 +80,7 @@ export let productosBD=[
         precio:55222000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/transition1.jpg?alt=media&token=d838db2a-8c9a-4a6a-9813-205efefbacd9','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/transition2.jpg?alt=media&token=799ece68-8da6-43eb-afec-e418aeed248a'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'La Transition Spur combina las líneas entre lo que una bicicleta XC debería ser capaz de hacer con la geometría de una bicicleta mucho más resistente. La transición ofrece un recorrido de suspensión delantera y trasera de 120 mm y ruedas de 29 ″.',
         popularidad:4
     },
     {
@@ -88,7 +88,7 @@ export let productosBD=[
         precio:7222000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/trek1.jpg?alt=media&token=3ac09f62-033f-40cc-9f5c-e103d722de39','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/trek2.jpg?alt=media&token=68eb3bf0-7418-47e6-bd8a-ba800b773c64'],
-        descripcion:'Bicicleta Gama Alta',
+        descripcion:'talla M (54), montada con grupo shimano ultegra 2x11v completo, cuadro de carbono OCLV 500, tija de sillin integrada de carbono, platos 52/36, manillar y potencia bontrager aluminio, biela izquierda shimano ultegra con potenciometro, llantas bontrager aeolus elite 35 carbono, ejes de 12mm.',
         popularidad:3
     },
     {
@@ -96,7 +96,7 @@ export let productosBD=[
         precio:75222000,
         cantidad:0,
         fotos:['https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/gw1.jpg?alt=media&token=95ee5c00-a9c8-4bf4-9421-0a324cc4aa21','https://firebasestorage.googleapis.com/v0/b/quiroz555tiendatcc.appspot.com/o/gw2.jpg?alt=media&token=743b10b4-a2c1-4182-b5b8-d59c68506c0d'],
-        descripcion:'Bicicleta Gama Baja con marco en hierro frenos mecanicos',
+        descripcion:'Bicicleta Gama Baja con marco en hierro frenos mecanicos suspension de recorrido 100mm',
         popularidad:2
     },
     ]
